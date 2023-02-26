ae387a7773218d8efdbf18a6f61546d1610cf11d
ayumu-uchiyama <118036137+ayumu-uchiyama@users.noreply.github.com>
2023-02-26 11:33:00 +0900

    helpers.py追加

--- app.py
+++ app.py
@@ -5,9 +5,9 @@ from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
 from tempfile import mkdtemp
 from werkzeug.security import check_password_hash, generate_password_hash
-
 import datetime
-from flask import *
+
+from helpers import login_required
 
 # Configure application
 app = Flask(__name__)
@@ -15,4 +15,3 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     return render_template("index.html")
-
